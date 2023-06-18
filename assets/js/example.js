f4a17a78272263d27252c116b071b53fc8c8c3de
Costel <costelmartinescu2000@gmail.com>
2023-06-18 13:54:41 +0300

    API

--- assets/js/example.js
+++ assets/js/example.js
@@ -1,8 +1,42 @@
-const API_KEY = "47de525c8b6e8be1118dc5ace4b3687a"; //should be private, but no server, nothing to hide btw.
 
-const city = "Arad";
-const source = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
 
-fetch(source)
-  .then((x) => x.text())
-  .then((y) => (document.getElementById("demo").innerHTML = y));
+function cautare() {
+
+  const API_KEY = "47de525c8b6e8be1118dc5ace4b3687a";
+  var input = document.getElementById("searchInput").value;
+
+
+  const source = `http://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${API_KEY}`;
+
+  fetch(source)
+
+    .then((response) => {
+      if (!response.ok) {
+        document.getElementById("status").innerHTML = "A apărut o eroare: " + response.status;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      //Deocamdata se afiseaza numele orasului cautat
+      document.getElementById("demo").innerHTML = data.city.name;
+
+      let istoricCautari = localStorage.getItem("istoricCautari");
+      if (!istoricCautari) {
+        istoricCautari = [];
+      } else {
+        istoricCautari = JSON.parse(istoricCautari);
+      }
+      istoricCautari.push(input);
+      localStorage.setItem("istoricCautari", JSON.stringify(istoricCautari));
+    })
+    .catch((error) => {
+      console.error("A apărut o eroare:", error);
+    });
+
+    
+}
+
+
+
+
+
